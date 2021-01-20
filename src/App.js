2a7a99c954565b6860b642e4ed290a1e95a3d269
khalilhamdii <khaalil.hamdi@gmail.com>
2021-01-20 23:51:28 +0100

    Fix linter errors and linter files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
-import PropTypes from "prop-types";
-import Big from "big.js";
+/* eslint-disable no-unused-vars */
+import './App.css';
+import PropTypes from 'prop-types';
+import Big from 'big.js';
+import React from 'react';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Math Magicians App</h1>
-      </header>
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Math Magicians App</h1>
+    </header>
+  </div>
+);
 
 export default App;
