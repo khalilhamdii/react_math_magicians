9caa3d5ca791dd6dfd05d16d03ecaf045199596c
khalilhamdii <khaalil.hamdi@gmail.com>
2021-01-26 00:02:07 +0100

    Refactor App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,29 @@
 /* eslint-disable react/no-unused-state */
 /* eslint-disable arrow-parens */
 import '../stylesheets/App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const App = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setnext] = useState(null);
+  const [operation, setoperation] = useState(null);
 
-  handleClick(btnName) {
-    const data = this.state;
+  const handleClick = (btnName) => {
+    const data = { total, next, operation };
     const modifiedData = calculate(data, btnName);
-    this.setState({
-      total: modifiedData.total,
-      next: modifiedData.next,
-      operation: modifiedData.operation,
-    });
-  }
-
-  render() {
-    const { total } = this.state;
-    return (
-      <>
-        <Display result={total} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+    setTotal(modifiedData.total);
+    setnext(modifiedData.next);
+    setoperation(modifiedData.operation);
+  };
+  return (
+    <>
+      <Display result={total} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
 
 export default App;
