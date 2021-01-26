1ac65782ae9b900d86556412942e3c626d7d1e9d
khalilhamdii <khaalil.hamdi@gmail.com>
2021-01-26 04:14:01 +0100

    Add Home and quote components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,45 +2,24 @@ import React from 'react';
 import '../stylesheets/style.css';
 
 const Footer = () => (
-  <nav
-    className="navbar navbar-light navbar-expand-md navigation-clean mb-5"
-    style={{ background: '#F5913E' }}>
-    <div className="container">
-      <a
-        className="navbar-brand"
-        href="/"
-        style={{ fontFamily: 'Aclonica, sans-serif', fontSize: '30px' }}>
-        Math Magicians
-      </a>
-      <button
-        data-toggle="collapse"
-        className="navbar-toggler"
-        type="button"
-        data-target="#navcol-1">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navcol-1">
-        <ul className="nav navbar-nav ml-auto font-weight-bold">
-          <li className="nav-item">
-            <a className="nav-link active" href="/" style={{ color: 'var(--dark)' }}>
-              HOME
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/" style={{ color: 'rgb(255,255,255)' }}>
-              CALCULATOR
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/" style={{ color: 'rgb(255,255,255)' }}>
-              QUOTE
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <div className="footer-basic mt-5" style={{ background: '#F5913E' }}>
+    <footer>
+      <ul className="list-inline" style={{ color: 'white' }}>
+        <li className="list-inline-item">
+          <a href="/">Home</a>
+        </li>
+        <li className="list-inline-item">
+          <a href="/">Calculator</a>
+        </li>
+        <li className="list-inline-item">
+          <a href="/">Quote</a>
+        </li>
+      </ul>
+      <p className="copyright" style={{ fontSize: '16px', color: 'var(--dark)' }}>
+        Math Magicians by Khalil Hamdi © 2021
+      </p>
+    </footer>
+  </div>
 );
 
 export default Footer;
---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../stylesheets/style.css';
+
+const Home = () => (
+  <div className="container">
+    <h1 style={{ color: 'rgb(255,255,255)' }}>Welcome to our page!</h1>
+    <p style={{ color: 'white', fontSize: '25px' }}>
+      <br />
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+      non proident, sunt inculpa qui o!cia deserunt mollit anim id est laborum.
+    </p>
+    <br />
+    <p style={{ color: 'white', fontSize: '25px' }}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+      non proident, sunt in culpa qui o!cia deserunt mollit anim id est laborum.
+    </p>
+    <br />
+  </div>
+);
+
+export default Home;
